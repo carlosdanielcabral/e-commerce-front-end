ffdc82fbf232f08f2e43a41324f17f26bf1a260f
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-04-07 19:15:10 -0300

    Correcao sidebar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,51 +7,20 @@ import AppContext from '../../context/AppContext';
 import Search from '../Search';
 import CartIcon from '../CartIcon';
 import ProfileOptions from '../ProfileOptions/ProfileOptions';
+import Sidebar from '../Sidebar/Sidebar';
 import './index.css';
 
 const profileStyle = { color: 'white', fontSize: '40px' };
 const gearStyle = { color: 'rgb(80, 80, 80)', fontSize: '16px', margin: '0 5px' };
-const houseIconStyle = { color: 'rgb(100, 100, 100)', fontSize: '20px' };
 const menuStyle = { color: 'white', fontSize: '26px' };
 
 const Header = () => {
-  const { darkMode, setDarkMode } = useContext(AppContext); 
+  const { darkMode, setDarkMode } = useContext(AppContext)
 
   return (
     <header className={ `${darkMode && 'header-darkmode'}` }>
       <section className="header-container">
-        <div className="sidebar-container">
-          <button type="button">
-            <AiOutlineMenu style={ menuStyle }/>
-          </button>
-
-          <div className="sidebar">
-            <div className="options">
-              <ul>
-                <li>
-                  <ProfileOptions />
-                </li>
-
-                <li>
-                  <button
-                    type="button"
-                    onClick={ () => setDarkMode(!darkMode) }
-                  >
-                    {
-                      darkMode
-                        ? <BsFillMoonFill />
-                        : <BsFillSunFill />
-                    }
-                  </button>
-                </li>
-
-                <li>
-                  <CartIcon /> Meu carrinho
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
+        <Sidebar />
         <section className="logo">
           <h1>E-commerce</h1>
         </section>
@@ -75,15 +44,18 @@ const Header = () => {
 
       <nav className="navigation-links">
         <Search />
-        <Link to="/">
-          <AiFillHome style={ houseIconStyle } />
-        </Link>
 
-        {/* <Link to="/search-products">
-          <BsSearch />
-        </Link> */}
+        <section className="links">
+          <Link to="/">
+            <AiFillHome className="home-icon" />
+          </Link>
 
-        <CartIcon />
+          {/* <Link to="/search-products">
+            <BsSearch />
+          </Link> */}
+
+          <CartIcon />
+        </section>
       </nav>
     </header>
     );
